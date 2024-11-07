5fc88317ef9a5c654f052c08d4a1e77755e355db
aiden <aiden@eximbay.com>
2024-11-07 17:54:19 +0900

    [init]: init project

---
+++ src/main/java/com/aiden/dojangstampback/DojangstampBackApplication.java
@@ -0,0 +1,13 @@
+package com.aiden.dojangstampback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojangstampBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DojangstampBackApplication.class, args);
+    }
+
+}
