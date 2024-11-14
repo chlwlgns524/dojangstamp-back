01157726c5bcd2956f227a8cc04ddf9a5cdf28eb
aiden <aiden@eximbay.com>
2024-11-14 17:32:40 +0900

    [fix]: StampJpaRepository 상속 추가

--- src/main/java/com/aiden/dojangstampback/domain/repository/stamp/StampJpaRepository.java
+++ src/main/java/com/aiden/dojangstampback/domain/repository/stamp/StampJpaRepository.java
@@ -3,5 +3,5 @@ package com.aiden.dojangstampback.domain.repository.stamp;
 import com.aiden.dojangstampback.domain.entity.Stamp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StampJpaRepository extends JpaRepository<Stamp, Long> {
+public interface StampJpaRepository extends JpaRepository<Stamp, Long>, StampRepository {
 }
