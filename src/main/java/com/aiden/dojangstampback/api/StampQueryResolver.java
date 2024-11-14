3e543620db381a0d1e0e370b84aaf1a7b7c98d27
aiden <aiden@eximbay.com>
2024-11-14 17:33:36 +0900

    [feat]: graphql 스키마 및 Order, Stamp QueryResolver 개발

--- src/main/java/com/aiden/dojangstampback/api/StampQueryResolver.java
+++ src/main/java/com/aiden/dojangstampback/api/StampQueryResolver.java
@@ -1,7 +1,7 @@
 package com.aiden.dojangstampback.api;
 
-import com.aiden.dojangstampback.domain.dto.OrderDto;
-import com.aiden.dojangstampback.domain.service.OrderService;
+import com.aiden.dojangstampback.domain.dto.StampDto;
+import com.aiden.dojangstampback.domain.service.StampService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -10,13 +10,13 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-public class OrderQueryResolver {
+public class StampQueryResolver {
 
-    private final OrderService orderService;
+    private final StampService stampService;
 
     @QueryMapping
-    public List<OrderDto> orderList() {
-        return orderService.findAllOrders();
+    public List<StampDto> stampList() {
+        return stampService.findAllStamps();
     }
 
 }
